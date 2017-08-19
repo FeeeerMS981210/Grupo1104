dad1cabaab97277820847189cb5ab28032a7d3f9
FeeeerMS981210 <31105265+FeeeerMS981210@users.noreply.github.com>
2017-08-19 14:01:27 -0500

    Update HolaMundo.c

--- HolaMundo.c
+++ HolaMundo.c
@@ -2,5 +2,13 @@
 
 int main(){
   printf("\nHola Mundo");
+  printf("\nFernanda Magaña\n");
   return 0;
 }
+#include<stdio.h> int main(){
+printf(“Este es mi segundo codigo”); 
+printf(“Programar es facil”);
+printf(“Voy a compartir con mi compañero el codigo”); 
+return 0;
+}	
+
